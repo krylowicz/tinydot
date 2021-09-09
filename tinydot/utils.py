4cc8d06245307a9a401ee99a2fb264ea4421d1ab
krylowicz <kacper@krylowicz.pl>
2021-09-09 13:39:15 +0200

    get value at index for nd tensor function

--- tinydot/utils.py
+++ tinydot/utils.py
@@ -8,3 +8,11 @@ def flatten(array):
       res.append(item)
   return res
 
+def get_value_at_index(coord, size):
+  mul = 1
+  index = 0
+  for i in range(len(size)):
+    index += mul * coord[i]
+    mul *= size[i]
+  return index
+
