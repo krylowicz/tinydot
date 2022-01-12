816d2d129e8cdd1eb784a20f4a2bbd7489c950b7
krylowicz <kacper@krylowicz.pl>
2022-01-12 10:35:37 +0100

    test reshape with unknown shape

--- test/test_tensor.py
+++ test/test_tensor.py
@@ -22,8 +22,11 @@ class TestTensor(unittest.TestCase):
       [3, 4],
       [5, 6]
     ])
-    tensor.reshape((2, 3))
+    tensor.reshape(2, 3)
     self.assertEqual(tensor.data, [[1.0, 2.0, 3.0], [4.0, 5.0, 6.0]])
+
+    tensor.reshape(-1, 6)
+    self.assertEqual(tensor.data, [[1., 2., 3., 4., 5., 6.]])
 
   def test_copy(self):
     t1 = Tensor.ones([3, 5, 2])
